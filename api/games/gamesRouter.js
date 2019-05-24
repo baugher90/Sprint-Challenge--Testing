6fdfcc3533b184e30d79885a1ee593def23e5233
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-24 13:29:01 -0400

    first post test written

--- api/games/gamesRouter.js
+++ api/games/gamesRouter.js
@@ -1,9 +1,14 @@
-const router = require('express').Router();
-const db = require('./gamesModel');
+const router = require("express").Router();
+const db = require("./gamesModel");
 
-router.get('/', async (req, res) => {
-    const rows = await db.getAll();
-  res.status(200).json({message:"here you go", rows});
-  });
+router.get("/", async (req, res) => {
+  const rows = await db.getAll();
+  res.status(200).json({ message: "here you go", rows });
+});
+
+router.post("/", async (req, res) => {
+  const game = await db.insert(req.body);
+  res.status(201).json(game);
+});
 
 module.exports = router;
--- api/games/gamesRouter.test.js
+++ api/games/gamesRouter.test.js
@@ -1,24 +1,41 @@
 const request = require("supertest");
-const router = require('../server');
+const router = require("../server");
 
 describe("gamesRouter.js", () => {
-    describe("endpoints", () => {
-        describe("GET/games", () => {
-            it("should return 200 OK", async () => {
-                await request(router).get("/games")
-                .set('Accept', 'application/json')
-                .expect(200);
-              });
-            it("should return JSON data", async () => {
-                await request(router).get("/games")
-                .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-            })
-            it("should be functional", async () => {
-                await request(router).get("/games")
-                .set('Accept', 'application/json')
-                .expect({message:"here you go", rows: []});
-              });
-        })
-    })
-})
+  describe("endpoints", () => {
+    describe("GET/games", () => {
+      it("should return 200 OK", async () => {
+        await request(router)
+          .get("/games")
+          .set("Accept", "application/json")
+          .expect(200);
+      });
+      it("should return JSON data", async () => {
+        await request(router)
+          .get("/games")
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/);
+      });
+      it("should be functional", async () => {
+        await request(router)
+          .get("/games")
+          .set("Accept", "application/json")
+          .expect({ message: "here you go", rows: [] });
+      });
+    });
+    describe("Post/games", () => {
+      it("should return json data", async () => {
+        let data = {
+          title: "Fortnight",
+          genre: "Dancing",
+          releaseYear: 2020
+        };
+        await request(router)
+          .post("/games")
+          .send(data)
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/);
+      });
+    });
+  });
+});
