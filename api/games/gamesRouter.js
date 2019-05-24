5d41ab02f974ef05e960bd5fdc195f59b765d54a
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-24 13:11:27 -0400

    first router test passed

--- api/games/gamesRouter.js
+++ api/games/gamesRouter.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const db = require('./gamesModel');
 
+router.get('/', async (req, res) => {
+    const rows = await db.getAll();
+  res.status(200).json(rows);
+  });
 
 module.exports = router;
--- api/games/gamesRouter.test.js
+++ api/games/gamesRouter.test.js
@@ -1,12 +1,14 @@
 const request = require("supertest");
-const server = require('../server');
+const router = require('../server');
 
 describe("gamesRouter.js", () => {
     describe("endpoints", () => {
         describe("GET/games", () => {
             it("should return 200 OK", async () => {
-                const res = await request(server).get("/games");
-                expect(res.status).toBe(200)
+                await request(router).get("/games")
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200);
               });
         })
     })
