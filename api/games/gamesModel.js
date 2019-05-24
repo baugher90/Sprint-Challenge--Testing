43763c6363233201ffa6cd935b7ddab75f98d97f
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-24 12:30:39 -0400

    getAll test passed

--- api/games/gamesModel.js
+++ api/games/gamesModel.js
@@ -2,9 +2,14 @@ const db = require('../../data/dbConfig');
 
 module.exports = {
   insert,
+  getAll,
 };
 
 async function insert(games) {
     const [id] = await db('games').insert(games,"id");
   return db('games').where({ id}).first();
+}
+
+function getAll() {
+    return db('games');
 }
